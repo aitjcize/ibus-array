beb536a4dd05dd05ff96164e6dbbb6be285a759b
Hiroshi Yui <hiroshiyui@gmail.com>
2011-10-02 00:57:25 +0800

    * Some works about make CMake work...

---
+++ CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/yhh/prjsrc/ibus-array-read-only
+# Build directory: /home/yhh/prjsrc/ibus-array-read-only
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(src)
+SUBDIRS(data)
+SUBDIRS(icons)
+SUBDIRS(po)
+SUBDIRS(setup)
+SUBDIRS(tools)
